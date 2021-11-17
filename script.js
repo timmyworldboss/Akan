2f2b49cb8ae8dde912130d72c343413ff9071601
timmyworldboss <timothy.ndung'u@student.moringaschool.com>
2021-11-17 10:12:08 +0300

    js modifications

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-var CC, YY, MM, DD, d, dayValue;
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-function validate() {
-  var genders = document.getElementsByName("gender");
-  if( document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
-     alert( "Please provide a valid year of birth! eg 2019" );
-     document.myForm.year.focus() ;
-     return false;
-  }
-  else if( document.myForm.month.value == "" || isNaN( document.myForm.month.value ) || 
-  document.myForm.month.value.length != 2 || document.myForm.month.value > 12  || document.myForm.month.value <= 0){
-     alert( "Please provide your month of birth! between 1 and 12" );
-     document.myForm.month.focus() ;
-     return false;
-  }
-  else if( document.myForm.date.value == "" || isNaN( document.myForm.month.value ) || 
-  document.myForm.month.value.length != 2|| document.myForm.date.value > 31 || document.myForm.date.value <= 0) {
-     alert( "Please provide a valid date that you were born in!" );
-     document.myForm.day.focus() ;
-     return false;
-  }
-  else if(genders[0].checked==false && genders[1].checked==false ) {
-      alert("You must select male or female");
-      return false;
-  }   
-  else{
-    return true ;
-  }
-  
-}
 
 function calculateDayValue(){
   year = document.getElementById("year").value;
@@ -86,3 +54,51 @@ function findName(){
   dayValue = calculateDayValue();
   getGender();
 }
+
+function akanFunction() {
+   var ay, bm, cd;
+   var male, female;
+ 
+   ay = parseInt(document.getElementById('year').value);
+   bm = parseInt(document.getElementById('month').value);
+   cd = parseInt(document.getElementById('dayOfMonth').value);
+ 
+   male = document.getElementById('rad1').checked;
+   female = document.getElementById('rad2').checked;
+ 
+   var year, month, day;
+   year = parseInt(ay);
+   month = parseInt(bm);
+   day = parseInt(cd);
+ 
+ 
+   var century = parseInt((year - 1) / 100 + 1);
+   var formula = (((26 * (month + 1) / 10)) + ((5 * year / 4)) + ((century / 4) - 2 * century - 1) + day) % 7;
+   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Afua", "Afua", "Ama"];
+   var dayOfMonth = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   var dayIndex = Math.floor(formula);
+ 
+   if (year > 0 && year < 2020) {
+     if (month > 0 && month < 13) {
+       if (day > 0 && day < 32) {
+         //MALE NAMES
+         if (male) {
+           document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex] + ' your Akan name is ' + maleNames[dayIndex]);
+         } else {
+           //FEMALE NAMES
+           document.getElementById("result").innerHTML = (' You were born on a: ' + dayOfMonth[dayIndex] + ' your Akan name is ' + femaleNames[dayIndex]);
+         }
+ 
+       } else {
+         alert("Input the correct date e.g 30");
+       }
+ 
+     } else {
+       alert("Input the correct month e.g 12");
+     }
+ 
+   } else {
+     alert("input the correct year e.g 2019");
+   }
+ }
